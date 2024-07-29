1c70be19006c0e4e90261bd2b7256b60eb69dfa3
koo9 <hanulo@kook9.site>
2024-07-29 15:46:20 +0900

    추가사항

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment(중가)',
+        tooltip: 'Increment(중가) ',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
